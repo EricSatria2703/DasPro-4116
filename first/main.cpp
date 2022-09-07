c65b22aa5f24e80780660c2649afe50b5f81def9
EricSatria2703 <113002546+EricSatria2703@users.noreply.github.com>
2022-09-07 11:48:38 +0700

    Add files via upload

---
+++ first/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello! \nEric Satria Pratama \nA11.2022.14601 \nJl Gedong Songo Timur \nSingle player game enjoyer" << endl;
+    return 0;
+}
